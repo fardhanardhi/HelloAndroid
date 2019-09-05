4ca9dc612c9e49c056720f030c11ad56fd1e4dda
Fardhan Ardhi <fa.dsg.id@gmail.com>
2019-09-05 14:17:34 +0700

    Activity lifecyle

--- app/src/main/java/helloandroid/polinema/ac/id/helloandroid/MainActivity.java
+++ app/src/main/java/helloandroid/polinema/ac/id/helloandroid/MainActivity.java
@@ -2,6 +2,7 @@ package helloandroid.polinema.ac.id.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"App on start",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,"App on stop",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this,"App on restart",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this,"App on resume",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this,"App on pause",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this,"App on destroy",Toast.LENGTH_SHORT).show();
     }
 }
